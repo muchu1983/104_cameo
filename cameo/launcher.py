1cedf0cf79484ef138a7493770279cbe75da46e3
muchu hsu <muchu1983@gmail.com>
2016-02-13 14:13:48 +0800

    initial commit

---
+++ cameo/launcher.py
@@ -0,0 +1,17 @@
+"""
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+"""
+程式進入點 (main)
+"""
+
+def entry_point():
+    pass
+
+
+if __name__ == '__main__':
+    entry_point()
