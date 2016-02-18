97cdf01ac306aefe8a23facf4917f8ee704ad0ff
muchu hsu <muchu1983@gmail.com>
2016-02-18 13:52:06 +0800

    do nothing

--- test/unit/test_selenium.py
+++ test/unit/test_selenium.py
@@ -34,11 +34,11 @@ class SeleniumTest(unittest.TestCase):
         chromedriver = ".\cameo_res\chromedriver.exe"
         driver = webdriver.Chrome(chromedriver)
         time.sleep(random.randint(5,10))
-        driver.get("https://www.indiegogo.com/explore#/browse/landing")
+        driver.get("https://www.indiegogo.com/projects/help-hanako-the-elephant--2#/")
         time.sleep(15)
         #cate_hrefs = driver.find_element_by_css_selector(".ng-scope > a")
         source = driver.page_source.encode("utf-8")
-        f = io.open("explore.html", "w+", encoding="utf-8")
+        f = io.open("animals.html", "w+", encoding="utf-8")
         f.write(unicode(source))
         f.close()
         
