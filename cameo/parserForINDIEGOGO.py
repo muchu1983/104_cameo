4c6b71600385aee300a26b4effe5d042709e73e9
muchu hsu <muchu1983@gmail.com>
2016-02-19 17:09:31 +0800

    add parser

---
+++ cameo/parserForINDIEGOGO.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+from scrapy import Selector
+"""
+從 local HTML 檔案解析資料
+"""
+class ParserForINDIEGOGO:
+    
+    def __init__(self):
+        pass
+    
+    def parseCategoryPage(self):
+        pass
