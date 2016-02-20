e5e0384b6f97e0ea058998f9bfc0108fbb72d2b7
wittehsieh <sengallery@gmail.com>
2016-02-20 21:00:42 +0800

    update sikuli

--- cameo_sikuli/spiderForINDIEGOGO.sikuli/spiderForINDIEGOGO.py
+++ cameo_sikuli/spiderForINDIEGOGO.sikuli/spiderForINDIEGOGO.py
@@ -96,7 +96,7 @@ def downloadProjectPages():
         projId = 0
         for urlLine in urlListFile:
             
-            if(projId < 161):
+            if(projId < 164):
                 projId = projId+1
                 continue
             
@@ -117,7 +117,7 @@ def downloadProjectPages():
                 type(Key.UP)
                 sleep(2)
             click("1455892865719.png")
-            wait("1455971639641.png", 20)
+            wait("1455973105407.png", 20)
             saveCurrentPage(str(projId) + ".html")
             projId = projId+1
         urlListFile.close()
