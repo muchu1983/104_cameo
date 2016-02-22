e971be1ad0b106c91557f6ce649609d79903ad53
wittehsieh <sengallery@gmail.com>
2016-02-22 13:58:30 +0800

    update sikuli

--- cameo_sikuli/spiderForINDIEGOGO.sikuli/spiderForINDIEGOGO.py
+++ cameo_sikuli/spiderForINDIEGOGO.sikuli/spiderForINDIEGOGO.py
@@ -110,7 +110,7 @@ def downloadProjectPages():
         intProjId = 0
         for strUrlLine in urlListFile:
             #continue point 
-            if(intProjId < 132):
+            if(intProjId < 261):
                 intProjId = intProjId+1 #skip
                 continue
             typeUrlOnChrome(urlText=strUrlLine)
