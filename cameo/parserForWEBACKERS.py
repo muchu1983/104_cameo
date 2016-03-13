777456a84687197b9ec621da3358becfc3da84f6
muchu hsu <muchu1983@gmail.com>
2016-03-14 01:40:20 +0800

    add parser for webackers

---
+++ cameo/parserForWEBACKERS.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+import os
+import datetime
+import re
+import json
+import logging
+from scrapy import Selector
+from cameo.utility import Utility
+"""
+從 source_html 的 HTML 檔案解析資料
+結果放置於 parsed_result 下
+"""
+class ParserForWEBACKERS:
+    #建構子
+    def __init__(self):
+        logging.basicConfig(level=logging.WARNING)
+        self.utility = Utility()
+        self.dicSubCommandHandler = {}
+        self.SOURCE_HTML_BASE_FOLDER_PATH = u"cameo_res\\source_html"
+        self.PARSED_RESULT_BASE_FOLDER_PATH = u"cameo_res\\parsed_result"
+        self.dicParsedResultOfProject = {} #project.json 資料
+        self.dicParsedResultOfUpdate = {} #update.json 資料
+        self.dicParsedResultOfComment = {} #comment.json 資料
+        self.dicParsedResultOfReward = {} #reward.json 資料
+        self.dicParsedResultOfProfile = {} #profile.json 資料
+        
+    #取得 parser 使用資訊
+    def getUseageMessage(self):
+        return """- WEBACKERS -
+useage:
+"""
+
+    #執行 parser
+    def runParser(self, lstSubcommand=[]):
+        pass
+
+#category #####################################################################################
+    #解析 category.html
+    def parseCategoryPage(self, uselessArg1=None):
+        
+        pass
---
+++ test/unit/test_parserForWEBACKERS.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+import unittest
+import logging
+import json
+from cameo.parserForWEBACKERS import ParserForWEBACKERS
+
+"""
+測試
+"""
+
+class ParserForWEBACKERSTest(unittest.TestCase):
+
+    #準備
+    def setUp(self):
+        logging.basicConfig(level=logging.WARNING)
+        pass
+        
+    #收尾
+    def tearDown(self):
+        pass
+        
+    #測試 解析 category 頁面
+    def test_parseCategoryPage(self):
+        logging.info("ParserForWEBACKERSTest.test_parseCategoryPage")
+        parser = ParserForWEBACKERS()
+        parser.parseCategoryPage()
+
+#測試開始
+if __name__ == "__main__":
+    unittest.main(exit=False)
+
+
