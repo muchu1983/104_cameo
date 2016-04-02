2d57c43800d75720f7e02f5ee30ddb5c78ca2172
muchu hsu <muchu1983@gmail.com>
2016-04-02 18:10:35 +0800

    add TECHORANGE spider parser localdb py files and tester py files

---
+++ test/unit/test_parserForTECHORANGE.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+import unittest
+import logging
+import json
+from cameo.parserForWEBACKERS import ParserForWEBACKERS
+
+"""
+測試
+"""
+
+class ParserForTECHORANGETest(unittest.TestCase):
+
+    #準備
+    def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+        pass
+        
+    #收尾
+    def tearDown(self):
+        pass
+    
+    #測試 解析 回饋組合的贊助狀態 字串
+    def test_parseStrRewardBacker(self):
+        logging.info("ParserForWEBACKERSTest.test_parseStrRewardBacker")
+        parser = ParserForWEBACKERS()
+        ret = parser.parseStrRewardBacker(strRewardBacker=u"1人待繳5人剩餘94人")
+        self.assertEquals((1, 5, 94), ret)
+        ret = parser.parseStrRewardBacker(strRewardBacker=u"1人待繳5人")
+        self.assertEquals((1, 5, None), ret)
+        ret = parser.parseStrRewardBacker(strRewardBacker=u"15人")
+        self.assertEquals((15, None, None), ret)
+    
+    #測試 解析 category 頁面
+    def test_parseCategoryPage(self):
+        logging.info("ParserForWEBACKERSTest.test_parseCategoryPage")
+        parser = ParserForWEBACKERS()
+        parser.parseCategoryPage()
+        
+    #測試 解析 project 頁面
+    def test_parseProjectPage(self):
+        logging.info("ParserForWEBACKERSTest.test_parseProjectPage")
+        strCategoryName = "design"
+        parser = ParserForWEBACKERS()
+        parser.beforeParseProjectPage(strCategoryName)
+        parser.parseIntroPage(strCategoryName)
+        parser.parseSponsorPage(strCategoryName)
+        parser.parseProgressPage(strCategoryName)
+        parser.parseFaqPage(strCategoryName)
+        parser.afterParseProjectPage(strCategoryName)
+    
+    #測試 解析 profile 頁面
+    def test_parseProfilePage(self):
+        logging.info("ParserForWEBACKERSTest.test_parseProfilePage")
+        strCategoryName = "design"
+        parser = ParserForWEBACKERS()
+        parser.beforeParseProfilePage(strCategoryName)
+        parser.parseProjPage(strCategoryName)
+        parser.parseOrderPage(strCategoryName)
+        parser.afterParseProfilePage(strCategoryName)
+
+#測試開始
+if __name__ == "__main__":
+    unittest.main(exit=False)
+
+
---
+++ test/unit/test_spiderForTECHORANGE.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2015, MuChu Hsu
+Contributed by Muchu Hsu (muchu1983@gmail.com)
+This file is part of BSD license
+
+<https://opensource.org/licenses/BSD-3-Clause>
+"""
+import unittest
+import logging
+from cameo.spiderForWEBACKERS import SpiderForWEBACKERS
+"""
+測試 抓取 WEBACKERS
+"""
+
+class SpiderForTECHORANGETest(unittest.TestCase):
+
+    #準備
+    def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+        self.spider = SpiderForWEBACKERS()
+        self.spider.initDriver()
+        
+    #收尾
+    def tearDown(self):
+        self.spider.quitDriver()
+    
+    #測試抓取 Browse page
+    def test_downloadBrowsePageAndParseBrowsePage(self):
+        logging.info("SpiderForWEBACKERSTest.test_downloadBrowsePageAndParseBrowsePage")
+        self.spider.downloadBrowsePageAndParseBrowsePage()
+
+    #測試抓取 Category page
+    def test_downloadCategoryPage(self):
+        logging.info("SpiderForWEBACKERSTest.test_downloadCategoryPage")
+        self.spider.downloadCategoryPage()
+    
+    #測試抓取 Project page
+    def test_downloadProjectPage(self):
+        logging.info("SpiderForWEBACKERSTest.test_downloadProjectPage")
+        self.spider.downloadProjectPage("sport")
+    
+    #測試抓取 profile page
+    def test_downloadProfilePage(self):
+        logging.info("SpiderForWEBACKERSTest.test_downloadProfilePage")
+        self.spider.downloadProfilePage("sport")
+    
+
+#測試開始
+if __name__ == "__main__":
+    unittest.main(exit=False)
+
+
