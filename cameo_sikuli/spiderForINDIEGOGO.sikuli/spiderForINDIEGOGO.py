1403747feb43f7fc04f2ec822fbe43396fdb2dc9
MuChu Hsu <muchu1983@gmail.com>
2016-05-06 01:49:20 +0800

    test x

--- cameo_sikuli/spiderForINDIEGOGO.sikuli/spiderForINDIEGOGO.py
+++ cameo_sikuli/spiderForINDIEGOGO.sikuli/spiderForINDIEGOGO.py
@@ -5,7 +5,7 @@ from java.awt import Toolkit
 from java.awt.datatransfer import StringSelection
 sysClipboard = Toolkit.getDefaultToolkit().getSystemClipboard()
 strBaseResFolderPath = r"C:\Users\Administrator\Desktop\pyWorkspace\CAMEO_git_code\cameo_res"
-appChrome = App("Google Chrome")
+appChrome = None
 dicPng = {"chrome_logo": "localpng_chrome_logo.png",
           "chrome_close":"chrome_close.png",
           "chrome_home":"chrome_home.png",
@@ -40,9 +40,9 @@ dicPng = {"chrome_logo": "localpng_chrome_logo.png",
           }
 #open chrome
 def openChrome():
-    if appChrome is not None:
-        appChrome.close()
-    appChrome = openApp("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe")
+    if spiderForINDIEGOGO.appChrome:
+        spiderForINDIEGOGO.appChrome.close()
+    spiderForINDIEGOGO.appChrome = openApp("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe")
     wait(dicPng["chrome_home"], 300)
     click(dicPng["chrome_home"])
     waitVanish(dicPng["chrome_stop"], 300)
