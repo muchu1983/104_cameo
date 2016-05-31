ad113fd44dd161b69a286961bf3e3ec578c98a88
MuChu Hsu <muchu1983@gmail.com>
2016-05-31 18:02:49 +0800

    add chmod clear read-only bit when rmtree run

--- cameo/cleaner.py
+++ cameo/cleaner.py
@@ -8,6 +8,7 @@ This file is part of BSD license
 """
 import shutil
 import os
+import stat
 """
 清理不需要的資料
 """
@@ -16,9 +17,13 @@ class CleanerForINDIEGOGO:
     def __init__(self):
         self.strBasedir = "cameo_res\\source_html\\INDIEGOGO"
     
+    def remove_readonly(funcRmtree, strPath, _):
+        os.chmod(strPath, stat.S_IWRITE)
+        funcRmtree(strPath)
+    
     def clean(self):
         for base, dirs, files in os.walk(self.strBasedir):
             for dir in dirs:
                 if dir.endswith("_files"):
                     strDirPath = base + "\\" + dir
-                    shutil.rmtree(strDirPath)
+                    shutil.rmtree(strDirPath, onerror=remove_readonly)
