b2fce5b5479d33e3980a9b3a12587f1193cd4318
MuChu Hsu <muchu1983@gmail.com>
2016-07-07 21:02:20 +0800

    mark:add exit to bat

--- cameo/automation/automationForWEBACKERS_executer.bat
+++ cameo/automation/automationForWEBACKERS_executer.bat
@@ -1,1 +1,2 @@
 start /dC:\Users\Administrator\Desktop\pyWorkspace\CAMEO_git_code cmd /k python C:\Users\Administrator\Desktop\pyWorkspace\CAMEO_git_code\cameo\automation\automationForWEBACKERS.py
+exit
