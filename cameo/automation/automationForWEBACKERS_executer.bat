f57fe68670666e1f5725cf520c989a0f24477981
MuChu Hsu <muchu1983@gmail.com>
2016-07-09 16:59:25 +0800

    auto mode bat remove PAUSE

--- cameo/automation/automationForWEBACKERS_executer.bat
+++ cameo/automation/automationForWEBACKERS_executer.bat
@@ -1,5 +1,4 @@
 CD C:\Users\Administrator\Desktop\pyWorkspace\CAMEO_git_code
 python C:\Users\Administrator\Desktop\pyWorkspace\CAMEO_git_code\cameo\automation\automationForWEBACKERS.py
 ECHO
-PAUSE
 EXIT
