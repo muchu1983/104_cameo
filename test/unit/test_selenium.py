60c5517bd39ab29a5e58e2e9d98f67152ba0ce44
MuChu Hsu <muchu1983@gmail.com>
2016-07-13 17:38:40 +0800

    add firefox selenium test

--- test/unit/test_selenium.py
+++ test/unit/test_selenium.py
@@ -8,6 +8,7 @@ This file is part of BSD license
 """
 import unittest
 import logging
+import time
 from selenium import webdriver
 
 """
@@ -24,9 +25,19 @@ class SeleniumTest(unittest.TestCase):
     #收尾
     def tearDown(self):
         pass
-
-    #測試 selenium
-    def test_selenium(self):
+    
+    #測試 firefox selenium
+    def test_firefox_selenium(self):
+        for i in range(100):
+            time.sleep(5)
+            driver = webdriver.Firefox()
+            driver.get("https://www.kickstarter.com/")
+            time.sleep(5)
+            driver.quit()
+            driver = None
+    
+    #測試 chrome selenium
+    def test_chrome_selenium(self):
         chromedriver = ".\cameo_res\chromedriver.exe"
         driver = webdriver.Chrome(chromedriver)
         driver.get("https://www.indiegogo.com/explorer")
@@ -34,6 +45,7 @@ class SeleniumTest(unittest.TestCase):
         #f = open("currency.html", "w+")
         #f.write(source.encode("utf-8"))
         #f.close()
+        driver.quit()
         
 #測試開始
 if __name__ == "__main__":
