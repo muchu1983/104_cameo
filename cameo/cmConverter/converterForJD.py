9726f2ba075583c971f0bd2fa0a47cf598336021
MuChu Hsu <muchu1983@gmail.com>
2016-09-06 16:24:09 +0800

    add jd converter project.json update.json comment.json okay

--- cameo/cmConverter/converterForJD.py
+++ cameo/cmConverter/converterForJD.py
@@ -9,13 +9,15 @@ This file is part of BSD license
 import logging
 import re
 import dateparser
-from crawlermaster.utility import Utility
+from cameo.utility import Utility as CameoUtility
+from crawlermaster.utility import Utility as CmUtility
 
 class ConverterForJD:
     
     #建構子
     def __init__(self):
-        self.cmUtility = Utility()
+        self.cmUtility = CmUtility()
+        self.cameoUtility = CameoUtility()
         self.dicParsedResultOfProject = {} #project.json 資料
         self.dicParsedResultOfUpdate = {} #update.json 資料
         self.dicParsedResultOfReward = {} #reward.json 資料
@@ -33,6 +35,8 @@ class ConverterForJD:
             strProjUrl = u"http://z.jd.com/project/details/%s.html"%strProjectId
             if strProjUrl not in self.dicParsedResultOfProject:
                 self.dicParsedResultOfProject[strProjUrl] = {}
+            if strProjUrl not in self.dicParsedResultOfReward:
+                self.dicParsedResultOfReward[strProjUrl] = {}
             # - 解析 project.json -
             #strSource
             self.dicParsedResultOfProject[strProjUrl]["strSource"] = u"JD"
@@ -42,31 +46,71 @@ class ConverterForJD:
             strCrawlTime = self.cmUtility.getCtimeOfFile(strFilePath=strIntroHtmlFilePath)
             self.dicParsedResultOfProject[strProjUrl]["strCrawlTime"] = strCrawlTime
             #strProjectName
+            strProjectName = self.cmUtility.extractFirstInList(lstSource=dicIntroPageRawData.get("jd-project-name", []))
+            self.dicParsedResultOfProject[strProjUrl]["strProjectName"] = strProjectName
             #strLocation
+            self.dicParsedResultOfProject[strProjUrl]["strLocation"] = u"China"
             #strCity
+            self.dicParsedResultOfProject[strProjUrl]["strCity"] = u"China"
             #strCountry
+            self.dicParsedResultOfProject[strProjUrl]["strCountry"] = u"CN"
             #strContinent
-            #strDescription
-            #strIntroduction
-            #intStatus
+            self.dicParsedResultOfProject[strProjUrl]["strContinent"] = u"AS"
+            #strDescription (全是圖檔)
+            self.dicParsedResultOfProject[strProjUrl]["strDescription"] = u""
+            #strIntroduction (全是圖檔)
+            self.dicParsedResultOfProject[strProjUrl]["strIntroduction"] = u""
             #strCreator
+            strCreator = self.cmUtility.extractFirstInList(lstSource=dicIntroPageRawData.get("jd-creator-name", []))
+            self.dicParsedResultOfProject[strProjUrl]["strCreator"] = strCreator.strip()
             #strCreatorUrl
+            strCreatorUrl = self.cmUtility.extractFirstInList(lstSource=dicIntroPageRawData.get("jd-creator-url", []))
+            self.dicParsedResultOfProject[strProjUrl]["strCreatorUrl"] = u"http://z.jd.com" + strCreatorUrl
             #strCategory
+            strCategory = re.search("JD\\\\(.*)\\\\projects\\\\[\d]+_intro.html$", strIntroHtmlFilePath).group(1)
+            self.dicParsedResultOfProject[strProjUrl]["strCategory"] = strCategory
             #strSubCategory
+            self.dicParsedResultOfProject[strProjUrl]["strSubCategory"] = strCategory
             #intFundTarget
+            strFundTarget = self.cameoUtility.stripTextArray(lstStrText=dicIntroPageRawData.get("jd-fund-target", []))
+            intFundTarget = int(re.sub("[^\d]", "", strFundTarget.strip()))
+            self.dicParsedResultOfProject[strProjUrl]["intFundTarget"] = intFundTarget
             #intRaisedMoney
+            strRaisedMoney = self.cmUtility.extractFirstInList(dicIntroPageRawData.get("jd-raised-money", [])).strip()
+            intRaisedMoney = int(strRaisedMoney)
+            self.dicParsedResultOfProject[strProjUrl]["intRaisedMoney"] = intRaisedMoney
             #fFundProgress
-            #strCurrency
+            fFundProgress = int((float(intRaisedMoney)/float(intFundTarget))*100.0)
+            self.dicParsedResultOfProject[strProjUrl]["fFundProgress"] = fFundProgress
             #intRemainDays
+            strRemainDays = self.cmUtility.extractFirstInList(dicIntroPageRawData.get("jd-remain-days", [])).strip()
+            intRemainDays = int(re.sub("[^\d]", "", strRemainDays.strip()))
+            self.dicParsedResultOfProject[strProjUrl]["intRemainDays"] = intRemainDays
+            #intStatus
+            intStatus = 0
+            if intRemainDays == 0:
+                if fFundProgress >= 100:
+                    intStatus = 1
+                else:
+                    intStatus = 2
+            self.dicParsedResultOfProject[strProjUrl]["intStatus"] = intStatus
+            #strCurrency
+            self.dicParsedResultOfProject[strProjUrl]["strCurrency"] = u"CNY"
             #strEndDate
-            #strStartDate
-            #intUpdate
-            #intBacker
-            #intComment
+            strEndDate = self.cmUtility.extractFirstInList(dicIntroPageRawData.get("jd-end-date", [])).strip()
+            strEndDate = self.cameoUtility.parseStrDateByDateparser(strOriginDate=strEndDate)
+            self.dicParsedResultOfProject[strProjUrl]["strEndDate"] = strEndDate
+            #strStartDate 無此資料
+            self.dicParsedResultOfProject[strProjUrl]["strStartDate"] = None
             #intFbLike
+            self.dicParsedResultOfProject[strProjUrl]["intFbLike"] = 0
             #intVideoCount
+            self.dicParsedResultOfProject[strProjUrl]["intVideoCount"] = 0
             #intImageCount
+            intImageCount = len(dicIntroPageRawData.get("jd-img-count", []))
+            self.dicParsedResultOfProject[strProjUrl]["intImageCount"] = intImageCount
             #isPMSelect 無法取得
+            self.dicParsedResultOfProject[strProjUrl]["isPMSelect"] = False
             #
             # - 解析 reward.json -
             #strUrl
@@ -76,12 +120,97 @@ class ConverterForJD:
             #intRewardLimit
             #strRewardShipTo and strRewardDeliveryDate
             #intRewardRetailPrice
-        #_progress.html raw data
+        #_progress.html raw data (update)
         lstDicProgressPageRawData = lstLstDicRawData[1]
-        #_qanda.html raw data
+        for dicProgressPageRawData in lstDicProgressPageRawData:
+            strProgressHtmlFilePath = dicProgressPageRawData.get("meta-data-html-filepath", None)
+            strProjectId = re.search("^.*\\\\([\d]+)_progress.html$", strProgressHtmlFilePath).group(1)
+            strProjUrl = u"http://z.jd.com/project/details/%s.html"%strProjectId
+            if strProjUrl not in self.dicParsedResultOfUpdate:
+                self.dicParsedResultOfUpdate[strProjUrl] = []
+            # - 解析 project.json -
+            #intUpdate
+            strUpdate = self.cmUtility.extractFirstInList(dicProgressPageRawData.get("jd-update", [])).strip()
+            intUpdate = int(re.sub("[^\d]", "", strUpdate.strip()))
+            self.dicParsedResultOfProject[strProjUrl]["intUpdate"] = intUpdate
+            # - 解析 update.json -
+            lstStrUpdateContent = dicProgressPageRawData.get("jd-update-content", [])
+            lstStrUpdateDate = dicProgressPageRawData.get("jd-update-date", [])
+            lstDicUpdateData = []
+            if len(lstStrUpdateContent) == len(lstStrUpdateDate):
+                for indexOfUpdate in range(len(lstStrUpdateContent)):
+                    dicUpdateData = {}
+                    #strUrl
+                    dicUpdateData["strUrl"] = strProjUrl
+                    #strUpdateContent
+                    dicUpdateData["strUpdateContent"] = lstStrUpdateContent[indexOfUpdate]
+                    #strUpdateDate
+                    strOriginUpdateDate = lstStrUpdateDate[indexOfUpdate].strip()
+                    strBaseDate = self.cmUtility.getCtimeOfFile(strFilePath=strProgressHtmlFilePath)
+                    strUpdateDate = self.cameoUtility.parseStrDateByDateparser(strOriginDate=strOriginUpdateDate, strBaseDate=strBaseDate)
+                    dicUpdateData["strUpdateDate"] = strUpdateDate
+                    #intComment 無此資料
+                    dicUpdateData["intComment"] = 0
+                    #intLike 無此資料
+                    dicUpdateData["intLike"] = 0
+                    #strUpdateTitle 無此資料
+                    dicUpdateData["strUpdateTitle"] = None
+                    lstDicUpdateData.append(dicUpdateData)
+            self.dicParsedResultOfUpdate[strProjUrl] = lstDicUpdateData
+        #_qanda.html raw data (comment)
         lstDicQandaPageRawData = lstLstDicRawData[2]
-        #_sponsor.html raw data
+        for dicQandaPageRawData in lstDicQandaPageRawData:
+            strQandaHtmlFilePath = dicQandaPageRawData.get("meta-data-html-filepath", None)
+            strProjectId = re.search("^.*\\\\([\d]+)_qanda.html$", strQandaHtmlFilePath).group(1)
+            strProjUrl = u"http://z.jd.com/project/details/%s.html"%strProjectId
+            if strProjUrl not in self.dicParsedResultOfComment:
+                self.dicParsedResultOfComment[strProjUrl] = []
+            if strProjUrl not in self.dicParsedResultOfQanda:
+                self.dicParsedResultOfQanda[strProjUrl] = []
+            # - 解析 project.json -
+            #intComment
+            strComment = self.cmUtility.extractFirstInList(dicQandaPageRawData.get("jd-comment", [])).strip()
+            intComment = int(re.sub("[^\d]", "", strComment.strip()))
+            self.dicParsedResultOfProject[strProjUrl]["intComment"] = intComment
+            # - 解析 comment.json -
+            lstStrCommentName = dicQandaPageRawData.get("jd-comment-name", [])
+            lstStrCommentDate = dicQandaPageRawData.get("jd-comment-date", [])
+            lstStrCommentContent = dicQandaPageRawData.get("jd-comment-content", [])
+            lstDicCommentData = []
+            if len(lstStrCommentName) == len(lstStrCommentDate) == len(lstStrCommentContent):
+                for indexOfComment in range(len(lstStrCommentName)):
+                    dicCommentData = {}
+                    #strUrl
+                    dicCommentData["strUrl"] = strProjUrl
+                    #isCreator
+                    dicCommentData["isCreator"] = False
+                    #strCommentName
+                    dicCommentData["strCommentName"] = lstStrCommentName[indexOfComment].strip()
+                    #strCommentDate
+                    strOriginCommentDate = lstStrCommentDate[indexOfComment].strip()
+                    strBaseDate = self.cmUtility.getCtimeOfFile(strFilePath=strQandaHtmlFilePath)
+                    strCommentDate = self.cameoUtility.parseStrDateByDateparser(strOriginDate=strOriginCommentDate, strBaseDate=strBaseDate)
+                    dicCommentData["strCommentDate"] = strCommentDate
+                    #strCommentContent
+                    dicCommentData["strCommentContent"] = lstStrCommentContent[indexOfComment].strip()
+                    lstDicCommentData.append(dicCommentData)
+            self.dicParsedResultOfComment[strProjUrl] = lstDicCommentData
+        #_sponsor.html raw data (backer)
         lstDicSponsorPageRawData = lstLstDicRawData[3]
+        for dicSponsorPageRawData in lstDicSponsorPageRawData:
+            strSponsorHtmlFilePath = dicSponsorPageRawData.get("meta-data-html-filepath", None)
+            strProjectId = re.search("^.*\\\\([\d]+)_sponsor.html$", strSponsorHtmlFilePath).group(1)
+            strProjUrl = u"http://z.jd.com/project/details/%s.html"%strProjectId
+            if strProjUrl not in self.dicParsedResultOfProject:
+                self.dicParsedResultOfProject[strProjUrl] = {}
+            # - 解析 project.json -
+            #intBacker
+            strBacker = self.cmUtility.extractFirstInList(dicSponsorPageRawData.get("jd-backer", [])).strip()
+            intBacker = int(re.sub("[^\d]", "", strBacker.strip()))
+            self.dicParsedResultOfProject[strProjUrl]["intBacker"] = intBacker
+            #lstStrBacker
+            lstStrBacker = dicSponsorPageRawData.get("jd-backer-name", [])
+            self.dicParsedResultOfProject[strProjUrl]["lstStrBacker"] = lstStrBacker
         
     #轉換 profile 資訊
     def convertProfile(self, lstLstDicRawData=[]):
@@ -122,7 +251,7 @@ class ConverterForJD:
             #lstStrCreatedProject
             self.dicParsedResultOfProfile[strProfUrl]["lstStrCreatedProject"] = dicProjPageRawData.get("jd-creator-CreatedProject", [])
             #lstStrCreatedProjectUrl
-            lstStrCreatedProjectUrl = self.stripAndCompleteJDUrlList(lstStrOriginUrl=dicProjPageRawData.get("jd-creator-CreatedProjectUrl", []))
+            lstStrCreatedProjectUrl = self.stripAndCompleteProjectUrlList(lstStrOriginUrl=dicProjPageRawData.get("jd-creator-CreatedProjectUrl", []))
             self.dicParsedResultOfProfile[strProfUrl]["lstStrCreatedProjectUrl"] = lstStrCreatedProjectUrl
             #intSuccessProject 無法取得
             self.dicParsedResultOfProfile[strProfUrl]["intSuccessProject"] = None
@@ -155,11 +284,11 @@ class ConverterForJD:
             #lstStrBackedProject
             self.dicParsedResultOfProfile[strProfUrl]["lstStrBackedProject"] = dicOrderPageRawData.get("jd-creator-BackedProject", [])
             #lstStrBackedProjectUrl
-            lstStrBackedProjectUrl = self.stripAndCompleteJDUrlList(lstStrOriginUrl=dicOrderPageRawData.get("jd-creator-BackedProjectUrl", []))
+            lstStrBackedProjectUrl = self.stripAndCompleteProjectUrlList(lstStrOriginUrl=dicOrderPageRawData.get("jd-creator-BackedProjectUrl", []))
             self.dicParsedResultOfProfile[strProfUrl]["lstStrBackedProjectUrl"] = lstStrBackedProjectUrl
         
     #去除 url list 中的 javascript:location.href 並加上 http://z.jd.com 組成完整的 JD url 
-    def stripAndCompleteJDUrlList(self, lstStrOriginUrl=[]):
+    def stripAndCompleteProjectUrlList(self, lstStrOriginUrl=[]):
         lstStrResultUrl = []
         for strOriginUrl in lstStrOriginUrl:
             pattern = re.compile("(/project/details/[\d]+\.html)")
