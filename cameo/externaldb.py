0647c03ffd8003d5c342d44083d704a677d0f370
MuChu Hsu <muchu1983@gmail.com>
2016-12-02 23:46:18 +0800

    move mongodb setting to cameo code

--- cameo/externaldb.py
+++ cameo/externaldb.py
@@ -6,10 +6,45 @@ This file is part of BSD license
 
 <https://opensource.org/licenses/BSD-3-Clause>
 """
-from bennu.externaldb import CameoMongoDb
+import logging
+from pymongo import MongoClient
 """
 外部資料庫存取
 """
+#外部資料庫 cameo 210.65.11.231
+class CameoMongoDb:
+    
+    #建構子
+    def __init__(self):
+        logging.info("connect to cameo 210.65.11.231 mongo db.")
+        self.client = MongoClient("mongodb://root:CameoRoot12#$@210.65.11.231/")
+        
+    #解構子
+    def __del__(self):
+        logging.info("close cameo 210.65.11.231 mongo db connection.")
+        self.client.close() #關閉資料庫連線
+        
+    #取得 mongodb client
+    def getClient(self):
+        return self.client
+        
+#外部資料庫 cameo 203.66.65.239
+class CameoMongoDbForMonitor:
+    
+    #建構子
+    def __init__(self):
+        logging.info("connect to cameo 203.66.65.239 mongo db.")
+        self.client = MongoClient("mongodb://203.66.65.239/")
+        
+    #解構子
+    def __del__(self):
+        logging.info("close cameo 203.66.65.239 mongo db connection.")
+        self.client.close() #關閉資料庫連線
+        
+    #取得 mongodb client
+    def getClient(self):
+        return self.client
+        
 #cameo mongodb tier
 class ExternalDbOfCameo:
     #建構子
